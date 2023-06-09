65204ab48d8d60abbeb5070bbd111fbfc1a134af
beejay <beejay.urzo@gmail.com>
2023-06-09 18:49:47 +0800

    All templates working

--- whatsapp-jumper-connector.js
+++ whatsapp-jumper-connector.js
@@ -10,7 +10,8 @@ const axios = require('axios');
 const qs = require('qs');
 const client = require("redis").createClient()
 const port = process.env.PORT || 3001;
-
+const callback_url = "https://test.urzo.online/jumper_callback"
+const moengage_callback = "https://test.urzo.online/moengage_callback"
 
 const findKeyValue = (obj, key, val) =>
   Object.keys(obj).filter(k => obj[key] === val && k ===key );
@@ -40,11 +41,68 @@ app.get('/', (req, res) => {
   res.json(200);
 });
 
+app.post('/moengage_callback', async (req, res) => {
+  console.log("moengage post",req.body)
+  res.send(200)
+})
 
-app.post('/jumper_callback', (req, res) => {
-  console.log("post",req.body)
+app.get('/list_jumper_templates', async (req, res) => {
+  console.log("moengage post",req.body)
+  var templates = await jumper_fetch_templates()
+  res.json(templates)
+})
+
+app.post('/jumper_callback', async (req, res) => {
+  console.log("post Callback: ")
+  console.dir(req.body, {depth:9})
+  payload = req.body.event
+  if(payload.subscription_type=="livechat"){
+    if(!payload.data.agent){ //means it's from the user
+      reply_message = await create_moengage_reply(payload.data.messageid, payload.data.conversationid,payload.data.message,payload.data.mobilecountrycode+payload.data.mobile)
+      let data = JSON.stringify(reply_message);
+      console.log("Data to be sent:", data)
+      let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: moengage_callback,
+        headers: { 
+          'Content-Type': 'application/json'
+        },
+        data : data
+      };
+      try {
+        const response = await axios.request(config);
+        console.log(response.data)
+        if (response.data.success == true){
+          return {"status":"success","message":response.data}
+        }
+        else return {"status":"error","message":"failed sending  callback to moengage"}
+      } catch (error) {
+        console.log(error);
+        return {"status":"error","message":"failed sending callback to moengage"}
+      }
+    }
+  }
+  
   res.json(200);
 });
+
+
+async function create_moengage_reply(message_id, conv_id, message, to){
+  var waba_number = await client.get("conv_id_waba_"+conv_id)
+  return {
+    "from": to,
+    "waba_number": waba_number,
+    "timestamp": Date.now(),
+    "type": "text",
+    "context": {
+      "msg_id": message_id
+    },
+    "text": { 
+      "body": message
+      },
+    }
+}
 
 app.get('/jumper_callback', (req, res) => {
   console.log("get",req.query)
@@ -67,7 +125,9 @@ app.post('/jumper_send_whatsapp', async (req, res) => {
         //if we find the language, let's send the message
         if(found_language.length>0){
           found=true
-          dat = await sendWhatsappMessage(template_languge.id,data.to)
+          components = null
+          if(data.template.components) components = data.template.components
+          dat = await sendWhatsappMessage(template_languge.id,data.to,data.msg_id, data.from, components)
           res.json(dat).end
         }
       })
@@ -89,14 +149,60 @@ app.post('/jumper_send_whatsapp', async (req, res) => {
 
 
 //send whatsapp message with template
-async function sendWhatsappMessage(template_id, number, options={}){
+async function sendWhatsappMessage(template_id, number, msg_id, waba_number,_components){
   socialChannels = await jumper_fetch_social_channels();
+
+  var components = {"HEADER":[],"BODY":[],"BUTTONS":[]}
+  if(_components){
+    for(comp of _components){
+      if(comp.type=="header"){
+        var p = new Array()
+        for(params of comp.parameters){
+          h = {}
+          link = params["image"]["link"]
+          h[link] = "image"
+          components["HEADER"].push(h)
+        }        
+      }
+      if(comp.type=="body"){
+        var p = new Array()
+        for(params of comp.parameters){
+          h = {}
+          if(params["type"]=="text"){
+            text = params["text"]
+            h[""+String(text)] = "text"
+            components["BODY"].push(h)
+          }      
+        }
+      }
+      if(comp.type=="button"){
+        // var p = new Array()
+        //   for (var i = 0; i < comp.parameters.length; i++) {
+        //     console.dir(comp.parameters[i], {depth:9})
+        //     if(comp.parameters[i]["type"] == "payload"){
+        //       comp.parameters[i]["type"] = "text"
+        //       comp.parameters[i]["text"] = comp.parameters[i]["payload"]
+        //       delete comp.parameters[i]["payload"]
+        //       console.dir(comp.parameters[i], {depth:9})
+        //     }
+        //   }
+          
+        
+        components["BUTTONS"].push(comp)
+      }
+    }
+  }
+
+  console.log("Generated Components")
+  console.dir(components, {depth:9})  
+
   let data = qs.stringify({
     'pageid': socialChannels.whatsapp.id,
     'conversationid': number,
      'channel':'whatsapp',
      'message':`s3ndt3mpl4te_${template_id}`,
-     'messagetype':"template"
+     'messagetype':"template",
+     'message_params':JSON.stringify(components)
   });
   console.log("Data to be sent:", data)
   let config = {
@@ -113,10 +219,15 @@ async function sendWhatsappMessage(template_id, number, options={}){
   try {
     const response = await axios.request(config);
     console.log(response.data)
-    return response.data
+    if (response.data.success == true){
+      client.set("message_id_"+response.data.message_id, msg_id)
+      client.set("conv_id_waba_"+response.data.conversationid, waba_number)
+      return {"status":"success","message":response.data}
+    }
+    else return {"status":"error","message":"failed sending message"}
   } catch (error) {
     console.log(error);
-    return null
+    return {"status":"error","message":"failed sending message"}
   }
 }
 
@@ -172,7 +283,6 @@ async function jumper_token(){
       return response.data.access_token
     } catch (error) {
       console.log(error);
-      callback(error)
       return null
     }
   }
@@ -193,7 +303,6 @@ async function jumper_fetch_social_channels(){
     return response.data
   } catch (error) {
     console.log(error);
-    callback(error)
     return null
   }
 }
@@ -213,7 +322,66 @@ async function jumper_fetch_templates(){
     return response.data.data
   } catch (error) {
     console.log(error);
-    callback(error)
+    return null
+  }
+}
+
+//Set callback subscription
+async function jumper_set_subscription(){
+  exisiting = false
+  subscriptions = await jumper_fetch_subscriptions()
+  if(subscriptions){
+    await subscriptions.forEach(async (sub) => {
+      found_webhook = findKeyValue(sub,"webhook", callback_url)
+      //if we find it, let's look if the language is supported by the template
+      if(found_webhook.length>0){
+        console.log("found existing webhook")
+        exisiting = true
+      }
+    });
+  }
+  if(exisiting){
+    return subscriptions
+  }
+  let data = qs.stringify({
+    'data': `[{"permission":"livechat","webhook":"${callback_url}"}]`
+  });
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://api.jumper.ai/app/add-subscription',
+    headers: { 
+      'Content-Type': 'application/x-www-form-urlencoded', 
+      'Authorization': `Bearer ${await jumper_token()}`
+    },
+    data : data
+  };
+
+  try {
+    const response = await axios.request(config);    
+    return response.data.data
+  } catch (error) {
+    console.log(error);
+    return null
+  }
+}
+
+//getch callback subscription
+async function jumper_fetch_subscriptions(){
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: 'https://api.jumper.ai/app/list-subscription',
+    headers: { 
+      'Authorization': `Bearer ${await jumper_token()}`
+    }
+  };
+
+  try {
+    const response = await axios.request(config);    
+    return response.data.data
+  } catch (error) {
+    console.log(error);
     return null
   }
 }
@@ -222,7 +390,8 @@ server.on('request', app)
 
 server.listen(port, async () => {
   console.log(`Starting server at port: ${port}`)
-  console.dir(await jumper_fetch_social_channels(), {depth:9})
+  
+  console.dir(await  jumper_set_subscription(), {depth:9})
 });
 
 // const localtunnel = require('localtunnel');

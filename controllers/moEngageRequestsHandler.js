a76ffd83f6bacb5b0d21ff0e9dacff7a52a10c89
sachin gavas <sachin.gavas@vonage.com>
2024-01-09 11:47:05 +0530

    changes for task queue

--- controllers/moEngageRequestsHandler.js
+++ controllers/moEngageRequestsHandler.js
@@ -1,14 +1,29 @@
 require('dotenv').config();
 const jumperMessage = require('./jumperMessage');
+const {addTask} = require('./http_task_que')
 
 const controller = {
   status: (req, res) => {
     console.log('status request called');
     res.status(200).send({data: 'Working fine'});
   },
-  execute: async (req, res) => {
-    console.log('execute request called', JSON.stringify(req.body));
+  sendMessage: async (req, res) => {
+    console.log('sendMessage request called', JSON.stringify(req.body));
     return await jumperMessage.sendWhatsAppMessage(req, res, req.body);
+  },
+  sendWhatsapp: async (req, res) => {
+    const project = process.env.DT_PROJECT_ID;
+    const queue = process.env.QUEUE_NAME;
+    const location = process.env.QUEUE_LOCATION;
+    const payload = JSON.stringify(req.body);
+
+    try {
+      await addTask(project, queue, location, payload);
+      res.status(200).send({status: "success"});
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({message: error});
+    }
   }
 };
 
--- router/index.js
+++ router/index.js
@@ -8,6 +8,7 @@ const jumperUser = require('../controllers/user');
 const taskQueue = require('../controllers/taskQueue');
 const social = require('../controllers/social');
 const auth = require('../controllers/auth');
+const jumperRequestHandler = require('../controllers/jumperRequestHandler');
 
 // routes for the services being called from UI
 router.post('/verifyJumperSavedToken', jumperToken.verifyJumperSavedToken);
@@ -15,7 +16,14 @@ router.post('/generateToken', jumperToken.generateToken);
 router.post('/newUser', jumperUser.newUser);
 
 // routes for the services being called from MoEngage
-router.get('/refresh_token', jumperToken.refreshToken);
+router.get('/refresh_token', jumperToken.refreshAllToken);
+router.get('/refresh_single_token', jumperToken.refreshToken);
+router.get('/list_jumper_templates', jumperTemplate.fetchWaTemplates);
+router.post('/jumper_callback', jumperRequestHandler.jumperCallback);
+router.post('/send_whatsapp_task_queue', taskQueue.moEngageTaskQueue);
+router.post('/jumper_send_whatsapp', jumperMessage.sendWhatsAppMessage);
+
+
 
 module.exports = router;
 

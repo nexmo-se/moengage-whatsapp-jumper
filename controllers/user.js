4b48e8d08b574742e6dd3e70fe1ce5c3de6e57a7
sachin gavas <sachin.gavas@vonage.com>
2024-01-10 17:52:21 +0530

    fix fetch user details

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,11 @@
-const {dt_store, dt_get, getUserDetailsBy_UID} = require('../datastore/datastore.js');
+const {dt_store, dt_get, getUserDetailsBy_UID, getUserDetailsBy_uid_shop_name} = require('../datastore/datastore.js');
 const util = require('../utils/util');
 const userModel = require('../model/user.js');
 
 const controller = {
   newUser: async (req, res) => {
     try {
-      const {user_uid, shop_name, token, refresh_token, client_key, secret_key, is_valid_token, mo_engage_jumper_app_token} = req.body;
+      const {user_uid, shop_name, token, refresh_token, client_key, secret_key, is_valid_token, mo_engage_jumper_app_token, dlr_web_hook_url} = req.body;
       const uid_shop_name = `${user_uid}_${shop_name}`;
       console.log('>>> new user', uid_shop_name);
 
@@ -13,7 +13,7 @@ const controller = {
         return res.sendStatus(400);
       }
       // console.log({kind: 'MOENGAGE_CONF', key: uid_shop_name, data: {uid_shop_name, token, refresh_token, client_key, secret_key, is_valid_token, token_expiry_time: util.dateTimeIso()}});
-      await dt_store({kind: 'MOENGAGE_CONF', key: uid_shop_name, data: {uid_shop_name, user_uid, shop_name, token, refresh_token, mo_engage_jumper_app_token, client_key, secret_key, is_valid_token, token_expiry_time: util.dateTimeIso()}});
+      await dt_store({kind: 'MOENGAGE_CONF', key: uid_shop_name, data: {uid_shop_name, user_uid, shop_name, token, refresh_token, mo_engage_jumper_app_token, client_key, secret_key, is_valid_token, dlr_web_hook_url, token_expiry_time: util.dateTimeIso()}});
       const updateTokenForUser = await userModel.refreshToken({token, refresh_token, client_key, secret_key});
       res.status(200).json({status: 'success', updateTokenForUser});
       // res.status(200).json({status: 'success'});
@@ -23,10 +23,10 @@ const controller = {
     }
   },
   user: async (req, res) => {
-    const {user_uid} = req.query;
-    const uid_shop_name = `${user_uid}_${req.params.shopName}`;
+    const uid_shop_name = `${req.query.userId}_${req.query.shopName}`;
+    console.log(uid_shop_name);
     try {
-      const data = await getUserDetailsBy_UID(uid_shop_name);
+      const data = await getUserDetailsBy_uid_shop_name(uid_shop_name);
       if (data) {
         return res.status(200).json(data);
       } else {
--- router/index.js
+++ router/index.js
@@ -9,11 +9,12 @@ const taskQueue = require('../controllers/taskQueue');
 const social = require('../controllers/social');
 const auth = require('../controllers/auth');
 const jumperRequestHandler = require('../controllers/jumperRequestHandler');
+const user = require('../controllers/user');
 
 // routes for the services being called from UI
 router.post('/verifyJumperSavedToken', jumperToken.verifyJumperSavedToken);
 router.post('/generateToken', jumperToken.generateToken);
-router.post('/newUser', jumperUser.newUser);
+router.post('/user', jumperUser.newUser);
 
 // routes for the services being called from MoEngage
 router.get('/refresh_token', jumperToken.refreshAllToken);
@@ -22,6 +23,7 @@ router.get('/list_jumper_templates', jumperTemplate.fetchWaTemplates);
 router.post('/jumper_callback', jumperRequestHandler.jumperCallback);
 router.post('/send_whatsapp_task_queue', taskQueue.moEngageTaskQueue);
 router.post('/jumper_send_whatsapp', jumperMessage.sendWhatsAppMessage);
+router.get('/user', user.user);
 
 
 

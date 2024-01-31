99a82ae8c6b030b48b4b00df6e6751d5ed077dec
sachin gavas <sachin.gavas@vonage.com>
2024-01-31 10:56:39 +0530

    US and EU deployment

---
+++ gcloudEU.sh
@@ -0,0 +1,7 @@
+# for latest docker image run "npm run docker" 
+
+export PROJECT_ID=jumper-147806
+export REGION=us-central1
+export SERVICENAME=moengage-eu
+
+gcloud builds submit --tag gcr.io/$PROJECT_ID/$SERVICENAME --project=$PROJECT_ID
